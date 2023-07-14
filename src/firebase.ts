0f7153340dca86efc6e323156cc2d9fb860912e3
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-07-14 11:37:40 -0300

    Configuração firebase

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { getDatabase } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: "https://cameras-ccc91-default-rtdb.firebaseio.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
+export { db }
