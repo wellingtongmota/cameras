46f720d90c00a9af696b87b87c874e3d5cb3a3da
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-07-14 14:47:34 -0300

    Atualização variáveis de ambiente

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_FIREBASE_API_KEY: string,
+  readonly VITE_APP_FIREBASE_AUTH_DOMAIN: string,
+  readonly VITE_APP_FIREBASE_DATABASE_URL: string,
+  readonly VITE_APP_PROJECT_ID: string,
+  readonly VITE_APP_STORAGE_BUCKET: string,
+  readonly VITE_APP_MESSAGING_SENDER_ID: string,
+  readonly VITE_APP__APP_ID: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
