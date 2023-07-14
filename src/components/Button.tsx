b594afa7e886556fe28ce360c251df558c18cd0b
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-07-14 11:51:01 -0300

    Criação componente Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps {
+  children: React.ReactNode,
+  color: string,
+  onHandleClick: (arg: any) => void
+}
+
+const Button = ({ children, color, onHandleClick }: ButtonProps) => {
+
+  const handleClick = (event: any) => onHandleClick(event)
+
+  return (
+    <button
+      className={"w-full h-20 text-lg lg:text-xl text-slate-50 hover:opacity-80 transition ease-in-out delay-75 rounded-md " + color}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
